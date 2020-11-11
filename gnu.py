203ff33af7d8c190e98ef24c8640063655a01025
Mars0J <70023143+Mars0J@users.noreply.github.com>
2020-11-12 00:23:24 +0300

    Add files via upload

---
+++ gnu.py
@@ -0,0 +1,55 @@
+from socket import *
+
+import os
+
+
+from colorama import Fore, Back, Style, init 
+init(autoreset=True) 
+
+os.system("clear")
+
+banner = """
+
+========================================================
+
+      ___           ___           ___           ___              
+     /\  \         /\  \         /\  \         /\  \             
+    /::\  \       /::\  \       /::\  \        \:\  \            
+   /:/\:\  \     /:/\:\  \     /:/\:\  \        \:\  \           
+  /::\~\:\  \   /:/  \:\  \   /::\~\:\  \       /::\  \          
+ /:/\:\ \:\__\ /:/__/ \:\__\ /:/\:\ \:\__\     /:/\:\__\         
+ \/__\:\/:/  / \:\  \ /:/  / \/_|::\/:/  /    /:/  \/__/         
+      \::/  /   \:\  /:/  /     |:|::/  /    /:/  /              
+       \/__/     \:\/:/  /      |:|\/__/     \/__/               
+                  \::/  /       |:|  |                           
+                   \/__/         \|__|                            
+
+=========================================================
+
+=SCAN V1.0 
+
+=CREATED BY MARS
+
+"""
+
+print (Fore.RED + banner)
+
+
+
+def scan():
+
+    if __name__ == '__main__':
+            hedef=input("Hedef: ")
+            hedef_ip=gethostbyname(hedef)
+            print("taranıyor:",hedef_ip)
+        
+            for i in range(6555):
+                baglantı=socket(AF_INET,SOCK_STREAM)
+                port_tarama=baglantı.connect_ex((hedef_ip,i))   
+                if(port_tarama==0):
+                   print ("Açık port: %d " %(i))
+                baglantı.close()
+                input("tarama sonlandırıldı.")
+
+scan()
+
